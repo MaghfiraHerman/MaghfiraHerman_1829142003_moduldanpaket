d1dbedc94874efec11b321e414fadfe23328cbb1
MaghfiraHerman <55131865+MaghfiraHerman@users.noreply.github.com>
2019-10-14 21:08:51 +0700

    Add files via upload

---
+++ BangunRuangDanBangunDatar.py
@@ -0,0 +1,29 @@
+#nama file: BangunRuangDanBangunDatar.py
+
+import math
+
+#fungsi keliling balok
+def kelilingBalok(p, l, t):
+    return 4 * (p + l + t)
+
+#fungsi volume balok
+def volumeBalok (p, l, t):
+    return p * l * t
+
+#fungsi luas jajar genjang
+def luasJajarGenjang(a, t):
+    return a * t
+
+#fungsi keliling jajar genjang
+def kelilingJajarGenjang(s):
+    return s + s + s + s
+
+#fungsi keliling kubus
+def kelilingKubus(s):
+    return 12 * s
+
+#fungsi volume kubus
+def volumeKubus(s):
+    return s * s * s
+
+
